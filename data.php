0653443543ff9d38126ae1d50e7954740025a449
Denperidge <denperidge@gmail.com>
2024-12-18 13:08:21 +0100

    First succesful regex

--- data.php
+++ data.php
@@ -2,6 +2,8 @@
 $GLOBALS["BASE_URL"] = "https://github.com/cypht-org/cypht/raw/master/config/";
 $GLOBALS["CACHE_DIR"] = ".cache-config-generator/";
 
+$REGEX_THE_BIG_ONE = '/(\/\*(?<comment>(.|\n)*?)\*\/(.|\n)*?)?env\(\'(?<key>.*?)\'(, *?(?<default>.*?))?\)/';
+$REGEX_REMOVE_FIRST_COMMENT = '/\/\*(.|\n)*?\*\//';
 $FILES = array("app.php", "database.php", "2fa.php", "carddav.php", "dynamic_login.php", "github.php", "ldap.php", "oauth2.php", "recaptcha.php", "wordpress.php");
 
 function getFile($filename) {
@@ -27,7 +29,35 @@ if (!is_dir($GLOBALS["CACHE_DIR"])) {
 
 for ($i = 0; $i < count($FILES); $i++) {
     $filename = $FILES[$i];
-    getFile($filename);
+    $contents = getFile($filename);
+
+    if ($filename == "app.php") {
+        $contents = preg_replace($REGEX_REMOVE_FIRST_COMMENT, "", $contents, 1);
+        continue;
+    }
+
+    $options = array();
+
+    preg_match_all(
+        $REGEX_THE_BIG_ONE, 
+        $contents,
+        $matches,
+        PREG_SET_ORDER);
+    foreach ($matches as $match) {        
+        #echo $match[0];
+        #echo "\n";
+        #echo $match[5];
+        /*
+        foreach ($match as $key => $value) {
+            echo  $key;
+            echo "\n";
+        };*/
+
+        array_push($options, (object) [
+            "key" => $match["key"],
+        ]);
+        break;
+    }
 }
 
 ?>
